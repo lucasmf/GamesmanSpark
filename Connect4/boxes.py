2a342c22f551fb0e5df4d9484fc40a36d8cd9200
Leticia Maia <leticiamaia@calvisitor-10-105-130-143.CalVisitor.1918.Berkeley.EDU>
2015-09-28 11:25:44 -0700

    Dando resultatltado

--- Connect4/boxes.py
+++ Connect4/boxes.py
@@ -4,11 +4,13 @@ initialPosition = 16 *"0" + 2*"0"
 initialGrid = 16 *"0" + 2*"0"
 #2*2*2*2 grid + pointsA + pointsB
 def primitive(position):
-    if position[0:16] == 16 * "1" and position[16] > position[16+1]:
+    flag = position[1] == "1" and position[2] == "1" and position[7] == "1" and position[11] == "1"
+    print flag
+    if flag and position[16] > position[16+1]:
         return G.WIN
-    elif position[0:16] == 16*"1" and position[16] < position[16+1]:
+    elif flag and position[16] < position[16+1]:
         return G.LOSE
-    elif position[0:16] == 16*"1":
+    elif flag:
         return G.TIE
     else:
         return G.UNDECIDED
@@ -22,33 +24,33 @@ def generateMoves(position):
         if idx < 2 :
             if position[idx*4+idx+2]=="0":
                 ret.append((idx, idx+2))
+    print ret
     return ret
 
 def doMove(position, move):
-    position[move[0]*4+move[1]] = "1"
+    position = position[0:move[0]*4+move[1]]+"1"+position[move[0]*4+move[1]+1:]
     if move[1] == move[0]+1:
         down1 = move[0] + 2
         down2 = move[1] + 2
         if position[move[0] * 4 + down1] =="1" and position[move[1] * 4 + down2] == "1" and position[down1 * 4 + down2] == "1":
-            position[16] += chr(ord(position[16])+1)
+            position = position[0:16]+chr(ord(position[16])+1)+position[17:]
         up1 = move[0] - 2
         up2 = move[1] - 2
         if position[up1 * 4 + move[0]] =="1" and position[up2 * 4 + move[1]] == "1" and position[up1 * 4 + up2] == "1":
-            position[16] += chr(ord(position[16])+1)
+            position = position[0:16]+chr(ord(position[16])+1)+position[17:]
                           
     elif move[1] == move[0]+2:
         right1 = move[0] + 1
         right2 = move[1] + 1
         if position[move[0] * 4 + right1] =="1" and position[move[1] * 4 + right2] == "1" and position[right1 * 4 + right2] == "1":
-            position[16] += chr(ord(position[16])+1)
+            position = position[0:16]+chr(ord(position[16])+1)+position[17:]
         left1 = move[0] - 1
         left2 = move[1] - 1
-        if position[left1 * 4 + move[0]] =="1" and position[left2 * 4 + move[1]] == "1" and position[left * 4 + up2] == "1":
-            position[16] += chr(ord(position[16])+1)
+        if position[left1 * 4 + move[0]] =="1" and position[left2 * 4 + move[1]] == "1" and position[left1 * 4 + left2] == "1":
+            position = position[0:16]+chr(ord(position[16])+1)+position[17:]
     else:
         var = position[16]
-        position[16] = position[16+1]
-        position[16+1] = var
+        position[16] = position[0:16]+position[16+1]+position[var]
     return position
 
 def solve(position=initialGrid):
